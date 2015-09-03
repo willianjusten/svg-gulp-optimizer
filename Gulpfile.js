7b73de3dafaef5e4a753c489253a93ae3a2eee46
willianjusten <willianjustenqui@gmail.com>
2015-09-02 23:46:45 -0300

    Add svgstore task

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,9 +1,18 @@
 var gulp  = require('gulp');
 var shell = require('gulp-shell');
+var svgstore = require('gulp-svgstore');
  
+// optimizer task
 gulp.task('shell', function () {
   return gulp.src('original/')
     .pipe(shell(
       'svgo -f original/ -o compressed/ --multipass -p 2'
     ));
+});
+
+gulp.task('svgstore', function () {
+    return gulp
+        .src('compressed/*.svg')
+        .pipe(svgstore())
+        .pipe(gulp.dest('build/'));
 });
